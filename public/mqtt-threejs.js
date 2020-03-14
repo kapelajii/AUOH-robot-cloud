6cd583755313c2d94fc934c65b6403c86ce978ab
FeikkoDev <you@example.com>
2020-03-14 20:15:11 +0200

    removed extra "t" from file name -added new renderer and empty scene

---
+++ public/mqtt-threejs.js
@@ -0,0 +1,45 @@
+// get correct div element from html-document
+let three_view = document.getElementById('three_view');
+
+// create new webGL renderer
+let renderer = new THREE.WebGLRenderer();
+
+// create new scene
+let scene = new THREE.Scene();
+
+// window settings
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+let view_angle = 45;
+
+// set clipping planes
+let near = 0.1 ; // meters
+let far = 1000; // meters
+
+/**
+ *  Crate new Perspective camera
+ *  Parameters:
+ *  PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
+ *  fov — Camera frustum vertical field of view.
+ *  aspect — Camera frustum aspect ratio.
+ *  near — Camera frustum near plane.
+ *  far — Camera frustum far plane. 
+ */
+
+let camera = new THREE.PerspectiveCamera( view_angle, width / height, near, far );
+// default camera is located in origo
+// move camera 5 meters in every direction
+camera.position.x = 5;
+camera.position.y = 5;
+camera.position.z = 5;
+
+renderer.setSize(width, height);
+three_view.appendChild(renderer.domElement);
+
+// draw function
+const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene,camera);
+};
+animate();
