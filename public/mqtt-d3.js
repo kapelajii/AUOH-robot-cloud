de6c14dc02cee0355daef0c36e119a864966097d
FeikkoDev <you@example.com>
2020-03-14 17:51:46 +0200

    Adde axis chart

---
+++ public/mqtt-d3.js
@@ -0,0 +1,202 @@
+let joint_datas = [];
+
+// Chart setings
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+let width = window.innerWidth - margin.left - margin.right;
+let height = window.innerHeight - margin.top - margin.bottom;
+
+const svg = d3
+  .select("#chart")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+let x_scale = d3.scaleTime().range([0, width]);
+
+let y_scale = d3
+  .scaleLinear()
+  .domain([-180, 180])
+  .range([height, 0]);
+  
+let g = svg
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//---------------------------
+// Setup joints for drawing
+//--------------------------
+
+// Joint 1   
+let j1 = d3
+  .line()
+  .x(d => {
+    return x_scale(Date.parse(d.time));
+  })
+  .y(d => {
+    return y_scale(d.joints[0]);
+  });
+ 
+// joint 2
+let j2 = d3
+  .line()
+  .x(d => {
+    return x_scale(Date.parse(d.time));
+  })
+  .y(d => {
+    return y_scale(d.joints[1]);
+  });
+
+// joint 3
+let j3 = d3
+.line()
+.x(d => {
+  return x_scale(Date.parse(d.time));
+})
+.y(d => {
+  return y_scale(d.joints[2]);
+});
+
+// joint 4
+let j4 = d3
+  .line()
+  .x(d => {
+    return x_scale(Date.parse(d.time));
+  })
+  .y(d => {
+    return y_scale(d.joints[3]);
+  });
+
+// joint 5
+let j5 = d3
+  .line()
+  .x(d => {
+    return x_scale(Date.parse(d.time));
+  })
+  .y(d => {
+    return y_scale(d.joints[4]);
+  });
+
+// joint 6
+let j6 = d3
+  .line()
+  .x(d => {
+    return x_scale(Date.parse(d.time));
+  })
+  .y(d => {
+    return y_scale(d.joints[5]);
+  });
+
+g.append("g").attr("class", "axis x");
+g.append("g").attr("class", "axis y");
+
+//-----------------------------------
+
+// Joint 1
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j1")
+    .attr("d", j1)
+    .style('fill', 'none')
+    .style('stroke', '#00F');
+
+// Joint 2
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j2")
+    .attr("d", j2)
+    .style('fill', 'none')
+    .style('stroke', '#0F0');
+
+// Joint 3
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j3")
+    .attr("d", j3)
+    .style('fill', 'none')
+    .style('stroke', '#F00');
+
+// Joint 4
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j4")
+    .attr("d", j4)
+    .style('fill', 'none')
+    .style('stroke', '#0FF');
+
+// Joint 5
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j5")
+    .attr("d", j5)
+    .style('fill', 'none')
+    .style('stroke', '#FF0');
+
+// Joint 6
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j6")
+    .attr("d", j6)
+    .style('fill', 'none')
+    .style('stroke', '#AAA');
+
+const resize = () => {
+  width = window.innerWidth - margin.left - margin.right;
+  height = window.innerHeight - margin.top - margin.bottom;
+  d3.select("#chart")
+    .select("svg")
+    .attr("width", width)
+    .attr("height", height);
+  x_scale.range([0, width]);
+  y_scale.range([height, 0]);
+  update_chart();
+};
+
+//update_chart()
+const update_chart = () => {
+    let x_max = Date.parse(joint_datas[joint_datas.length - 1].time);
+    let x_min = x_max - 30000;
+    x_scale.domain([x_min, x_max]);
+    g.select("g.axis.x")
+        .attr("transform", "translate(0," + (height - margin.bottom - margin.top) + ")")
+        .call(d3.axisBottom(x_scale).ticks(5));
+    g.select("g.axis.y")
+        .attr("class", "axis y")
+        .call(d3.axisLeft(y_scale));
+    svg.select(".j1") // change the line
+        .attr("d", j1(joint_datas));
+
+    svg.select(".j2") // change the line
+        .attr("d", j2(joint_datas));
+
+    svg.select(".j3") // change the line
+        .attr("d", j3(joint_datas));
+
+    svg.select(".j4") // change the line
+        .attr("d", j4(joint_datas));
+
+    svg.select(".j5") // change the line
+        .attr("d", j5(joint_datas));
+
+    svg.select(".j6") // change the line
+        .attr("d", j6(joint_datas));
+};
+
+
+// get data from MQTT server
+const mqtt_client = mqtt.connect("wss://auoh20-mqtt-broker.herokuapp.com/");
+mqtt_client.on("connect", () => {
+  console.log("connected to mqtt broker");
+  mqtt_client.subscribe("joints");
+});
+
+mqtt_client.on("message", (topic, message) => {
+  const joint_data = JSON.parse(message);
+  joint_datas.push(joint_data);
+  update_chart();
+});
